23b2abf30eeca8377cd2af7e54af0c9d62a1433d
Calvin Chestnut <me@icalvin.dev>
2023-02-15 20:56:51 -0500

    Initial Commit

---
+++ app.lol/app_lolApp.swift
@@ -0,0 +1,21 @@
+//
+//  app_lolApp.swift
+//  app.lol
+//
+//  Created by Calvin Chestnut on 2/12/23.
+//
+
+import UIDotAppDotLOL
+import SwiftUI
+
+@main
+public struct AppDotLOL: App {
+    private let appUI = UIDotAppDotLOL()
+    
+    public init() {
+    }
+    
+    public var body: some Scene {
+        appUI.body
+    }
+}
